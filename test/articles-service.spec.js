e5faac193c5f630344b84f535507a8f808d04486
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2020-10-23 16:25:58 -0500

    ArticlesService passing test for getAllArticles method

--- test/articles-service.spec.js
+++ test/articles-service.spec.js
@@ -1,5 +1,56 @@
+const ArticlesService = require('../src/articles-service');
+const knex = require('knex');
+const { expect } = require('chai');
+
 describe(`Articles service object`, () => {
-  it(`should run the tests`, () => {
-    expect(true).to.eql(false);
+  let db;
+  let testArticles = [
+    {
+      id: 1,
+      date_published: new Date('2029-01-22T16:28:32.615Z'),
+      title: 'First test post!',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?'
+    },
+    {
+      id: 2,
+      date_published: new Date('2100-05-22T16:28:32.615Z'),
+      title: 'Second test post!',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum.'
+    },
+    {
+      id: 3,
+      date_published: new Date('1919-12-22T16:28:32.615Z'),
+      title: 'Third test post!',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, voluptate? Necessitatibus, reiciendis? Cupiditate totam laborum esse animi ratione ipsa dignissimos laboriosam eos similique cumque. Est nostrum esse porro id quaerat.'
+    },
+  ];
+
+  before(() => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL,
+    });
+  });
+
+  // remove the data from table from previous test
+  before(() => db('blogful_articles').truncate());
+
+  before(() => {
+    return db
+      .into('blogful_articles')
+      .insert(testArticles)
+  });
+
+  // closes database connection after tests are done so you don't have to  Ctrl+C in terminal to kill it 
+  after(() => db.destroy());
+
+  describe(`getAllArticles()`, () => {
+    it(`resolves all articles from 'blogful_articles' table`, () => {
+      // test that ArticlesService.getAllArticles gets data from table
+      return ArticlesService.getAllArticles(db)
+        .then(actual => {
+          expect(actual).to.eql(testArticles)
+        });
+    });
   });
 });
