cb4dec9edc695261c8585953847c59603d4a95ba
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2020-10-26 18:06:38 -0500

    getAllItems() passing with test data

--- src/shopping-service.js
+++ src/shopping-service.js
@@ -1,8 +1,9 @@
 //TODO methods for CRUD: to get, insert, update and delete shopping list items
 
+// ShoppingService is a "Service Object", which is just one way for organizing this code and bundling/encapsulating several methods together to create cleaner, more reusable code for CRUD methods. 
 const ShoppingService = {
-  getAllItems() {
-    return 'all the items!'
+  getAllItems(knex) {
+    return knex.select('*').from('shopping_list')
   },
 };
 
--- test/shopping-list-service.spec.js
+++ test/shopping-list-service.spec.js
@@ -1,1 +1,62 @@
-//TODO tests the CRUD methods.
+const { expect } = require("chai");
+const ShoppingService = require('../src/shopping-service');
+const knex = require('knex');
+
+
+describe('ShoppingService Service Object', () => {
+  let db;
+  let testItems = [
+    {
+      id: 1,
+      name: 'Ghost Beef',
+      price: '4.33',
+      date_added: new Date('2029-01-22T16:28:32.615Z'),
+      checked: false,
+      category: 'Main'
+    },
+    {
+      id: 2,
+      name: 'Cheatloaf',
+      price: '5.00',
+      date_added: new Date('2100-05-22T16:28:32.615Z'),
+      checked: true,
+      category: 'Main'
+    },
+    {
+      id: 3,
+      name: 'Fried Trickin',
+      price: '6.40',
+      date_added: new Date('1919-12-22T16:28:32.615Z'),
+      checked: false,
+      category: 'Snack'
+    },
+  ];
+
+  before(() => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL,
+    });
+  });
+
+  before(() => db('shopping_list').truncate());
+
+  before(() => {
+    return db
+      .into('shopping_list')
+      .insert(testItems)
+  });
+
+  after(() => db.destroy());
+
+
+  describe('getAllItems() method', () => {
+
+    it('resolves all items from "shopping_list" table', () => {
+      return ShoppingService.getAllItems(db)
+        .then(actual => {
+          expect(actual).to.eql(testItems)
+        });
+    });
+  });
+});
